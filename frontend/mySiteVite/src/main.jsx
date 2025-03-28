e71ad8c26f7f7ee4d20c1d52900d86131486f84e
Priyanka <priya94@live.ca>
2025-03-27 23:57:23 -0400

    Frontend component initial setup

--- frontend/mySiteVite/src/main.jsx
+++ frontend/mySiteVite/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//import { Provider } from "@/components/ui/provider"
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 )
