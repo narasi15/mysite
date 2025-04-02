ef16fdd715648c679b626592c59961a31d9f02a3
Priyanka <priya94@live.ca>
2025-04-01 23:49:47 -0400

    Created Homepage with no products listed

--- frontend/mySiteVite/src/pages/HomePage.jsx
+++ frontend/mySiteVite/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Container, VStack, Text } from '@chakra-ui/react'
+import {Link } from 'react-router-dom'
 import { px } from 'framer-motion'
 import React from 'react'
 
@@ -9,6 +10,17 @@ const HomePage = () => {
   
         <Text fontSize="30px" fontWeight="bold" color="blue.500" textAlign="center"> 
         Current Products </Text>
+
+        <Text fontSize={'xl'} textAlign={'center'} fontWeight={'bold'} color={'gray.500'}>
+          No products found 🙁{""}
+          <Link to={"/create"}>
+            <Text as={'span'} color={'blue.500'} _hover={{textDecoration: "underline"}}>
+              Create a product
+
+            </Text>
+          </Link>
+
+        </Text>
 
 
       </VStack>
