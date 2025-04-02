ae6bb751a0317fdcb3fb5d3fa7ab692d03c9ed8a
Priyanka <priya94@live.ca>
2025-04-01 23:57:40 -0400

    Function created to populate homepage with products list

--- frontend/mySiteVite/src/pages/HomePage.jsx
+++ frontend/mySiteVite/src/pages/HomePage.jsx
@@ -1,15 +1,37 @@
-import { Container, VStack, Text } from '@chakra-ui/react'
+import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react'
 import {Link } from 'react-router-dom'
 import { px } from 'framer-motion'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useProductStore } from '@/store/product'
 
 const HomePage = () => {
+
+  const {fetchProducts, products} = useProductStore();
+
+  useEffect(() => {fetchProducts();}, [fetchProducts]);
+  console.log("products", products);
+
+
   return (
     <Container maxW='container.x1' py={12}>
       <VStack spacing={8}>
   
         <Text fontSize="30px" fontWeight="bold" color="blue.500" textAlign="center"> 
         Current Products </Text>
+
+        <SimpleGrid
+          columns={{
+            base:1, 
+            md: 2, 
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}>
+
+        </SimpleGrid>
+
+
+
 
         <Text fontSize={'xl'} textAlign={'center'} fontWeight={'bold'} color={'gray.500'}>
           No products found 🙁{""}
